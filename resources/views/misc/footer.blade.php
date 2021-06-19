4ab11950cd455f0b88306bcb82b66e3b6b529bab
Danica Jurić <75320340+danicajuric@users.noreply.github.com>
2021-06-20 01:04:31 +0200

    created footer

---
+++ resources/views/misc/footer.blade.php
@@ -0,0 +1,68 @@
+<div class="mojContainer">
+<footer class="navbar is-transparent is-boxed is-fixed-bottom has-text-centered">
+  <div class="navbar-item ">
+    
+      <span class="person">
+      <img src="storage\app\public\assets\gabi.png">
+      <a class="name">Gabrijela</a>
+      </span>
+      <span class="person">
+      <img src="storage\app\public\assets\ankica.png">
+      <a class="name">Ankica</a>
+      </span>
+      <span class="person">
+      <img src="storage\app\public\assets\tin.png">
+      <a class="name">Tin</a>
+      </span>
+      <span class="person">
+      <img src="storage\app\public\assets\marin.png">
+      <a class="name">Marin</a>
+      </span>
+      <span class="person">
+      <img src="storage\app\public\assets\danica.png">
+      <a class="name">Danica</a>
+      </span>
+    
+  </div>
+</footer>
+<!--CARTOON STYLE AVATARS: <a href='https://www.freepik.com/free-photos-vectors/people'>People vector created by macrovector - www.freepik.com</a>
+    ATRIBUTION FOR FREE ICONS?-->
+</div>
+
+<style scoped>
+  /*za sve str*/
+  body {
+    font-family: 'Roboto', sans-serif;
+    
+  }
+.navbar{
+      max-width:100%;
+      
+      background-color: #40826d;
+   
+  }
+.navbar-item{
+    margin:auto;
+    color: white;
+    font-family: 'Poppins', sans-serif;
+    font-size: 20px;
+    text-align:center;
+}
+.navbar-item a {
+    color: white;
+    text-shadow: 2px 2px 5px white;
+    font-weight:bold;
+  }
+.navbar-item a:hover {
+    color: #274f42;
+    text-shadow: 2px 2px 5px white;
+
+  }
+.person{
+    padding-left:20px;
+    padding-right:20px;
+}
+img{
+    height:183px;
+}
+</style>
