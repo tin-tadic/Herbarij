a9ead9e6edde2f4060363c934384881c1b66ee47
gj1827 <josipovic1501@gmail.com>
2021-07-05 21:16:25 +0200

    pagination with bulma added

--- resources/views/customers/customers.blade.php
+++ resources/views/customers/customers.blade.php
@@ -10,39 +10,42 @@
       <br><br>
       <div class="columns mt-5 is-8 is-variable is-centered is-multiline">
         @foreach ($buyers as $buyer)
-          <div class="column is-4-tablet is-3-desktop">
-            <div class="card">
-              <div class="card-image has-text-centered px-6">
-                <img src="storage/assets/user.png" alt="customer image">
-              </div>
-              <div class="card-content">
-                <p>{{ $buyer->ime }}</p>
-                <p>{{ $buyer->adresa }}</p>
-                <p>{{ $buyer->tip }}</p>
-              </div>
-              <footer class="card-footer">
-                <p class="card-footer-item">
-                  <button onclick="location.href='{{ route('getBuyerForEdit', $buyer->id) }}'" type="button" class="button mt-5" id="edit"> Edit </button>
-                  <button class="button mt-5" id="delete" onclick="event.preventDefault();
+        <div class="column is-4-tablet is-3-desktop">
+          <div class="card">
+            <div class="card-image has-text-centered px-6">
+              <img src="storage/assets/user.png" alt="customer image">
+            </div>
+            <div class="card-content">
+              <p>{{ $buyer->ime }}</p>
+              <p>{{ $buyer->adresa }}</p>
+              <p>{{ $buyer->tip }}</p>
+            </div>
+            <footer class="card-footer">
+              <p class="card-footer-item">
+                <button onclick="location.href='{{ route('getBuyerForEdit', $buyer->id) }}'" type="button" class="button mt-5" id="edit"> Edit </button>
+                <button class="button mt-5" id="delete" onclick="event.preventDefault();
                   if(confirm('Jeste li sigurni da želite obrisati kupca?')) {
                     document.getElementById(`deleteBuyer_{{ $buyer->id }}`).submit();
                   }">Delete
-                  </button>
-                  <form id="deleteBuyer_{{ $buyer->id }}" action="{{ route('deleteBuyer', ['buyerId' => $buyer->id]) }}" method="POST"> @csrf </form>
-                </p>
-              </footer>
-            </div>
+                </button>
+              <form id="deleteBuyer_{{ $buyer->id }}" action="{{ route('deleteBuyer', ['buyerId' => $buyer->id]) }}" method="POST"> @csrf </form>
+              </p>
+            </footer>
           </div>
-        @endforeach
-        <!--pagination-->
-        <div>
-          {{ $buyers->links() }}
         </div>
-        
+        @endforeach
+
+
       </div>
     </div>
   </section>
-  <button class="button mt-4" id="addCostumer" onclick="location.href='{{ route('addBuyer') }}'">Dodaj Kupca</button>
+  <div>
+    <button class="button mt-4" id="addCostumer" onclick="location.href='{{ route('addBuyer') }}'">Dodaj Kupca</button>
+    <!--pagination-->
+    <div class="mb-5 pb-10">
+      {{ $buyers->links() }}
+    </div>
+  </div>
 </div>
 <!-- modal za brisanje -->
 <div class="modal" id="modalDelete">
@@ -55,7 +58,7 @@
           <button class="button is-link is-success is-pulled-right" id="canceldelete">Cancel</button>
         </div>
         <div class="field">
-          <button class="button is-link is-danger is-pulled-left" >Delete</button>
+          <button class="button is-link is-danger is-pulled-left">Delete</button>
         </div>
       </form>
     </div>
@@ -68,9 +71,9 @@
   //buttons
   const dodajButton = document.querySelector('#dodaj');
   const editButton = document.querySelector('#edit');
-  const deleteButton = document.querySelector('#delete'); 
+  const deleteButton = document.querySelector('#delete');
   const deleteBuyerButton = document.querySelector('#deletebuyer');
-  
+
   const canceleditButton = document.querySelector('#canceledit');
   const canceldodajButton = document.querySelector('#canceldodaj');
   const canceldeleteButton = document.querySelector('#canceldelete');
@@ -83,14 +86,13 @@
   const modalDeleteBuyer = document.querySelector('#modalDeleteBuyer')
 
   //event listeners
-  deleteButton.addEventListener('click', ()=>{
+  deleteButton.addEventListener('click', () => {
     modalDelete.classList.add('is-active');
   })
 
-  deleteBuyerButton.addEventListener('click', ()=>{
+  deleteBuyerButton.addEventListener('click', () => {
     modalDeleteBuyer.classList.app('is-active');
   })
-  
 </script>
 <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 @endsection
